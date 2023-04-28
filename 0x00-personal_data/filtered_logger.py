0bec5b6a868f003ee7572ea9888e57ee34f4aab2
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-28 20:31:16 +0100

    Change annotation of fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,8 @@ import logging
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """Obfuscates a selected fieldsnin a string"""
+    """Obfuscates a selected fieldsnin a string
+    """
     for field in fields:
         message = re.sub(f"{field}=.*?{seperator}",
                          f"{field}={redaction}{separator}", message)
@@ -24,7 +25,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List) -> None:
+    def __init__(self, fields: List[str]) -> None:
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
