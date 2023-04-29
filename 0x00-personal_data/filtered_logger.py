a0f88465e2eb10e1af4c82a43b61a9c97c7785e5
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-29 14:20:34 +0100

    Change PII Fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import logging
 import os
 import mysql.connector
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
