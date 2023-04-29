c9ebda0338ac76fbb12ce147dfa5524d527bb863
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-29 14:57:17 +0100

    Validator function for hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Returns a hashed salted password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a hash was generated from a password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
