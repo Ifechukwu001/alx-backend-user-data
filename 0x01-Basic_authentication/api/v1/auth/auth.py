150e57fa97ef4f6d08caec3cb3d0b2331d9405c4
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-02 14:37:33 +0100

    Add docs for the methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,22 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require_auth method
+        Returns:
+            bool: False
+        """
         return False
     
     def authorization_header(self, request=None) -> str:
+        """authorization_header method
+        Returns:
+            str: None
+        """
         return None
     
     def current_user(self, request=None) -> TypeVar('User'):
+        """current_user method
+        Returns:
+            User: None
+        """
         return None
