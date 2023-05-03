27f525de97d5fa579584c5a865a63d4d5c707df5
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-03 05:46:26 +0100

    Decode a base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,13 +2,16 @@
 """Basic Authentication Module
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
     """BasicAuth Class
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """Extracts the Base64 part of the AUth header
         Returns:
             str: Base64 string
@@ -21,3 +24,20 @@ class BasicAuth(Auth):
             return None
         b64 = authorization_header.split(" ")[1]
         return b64
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Decodes a base64 header value
+        Returns:
+            str: The decoded string
+        """
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded_msg = base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        return decoded_msg.decode("utf-8")
