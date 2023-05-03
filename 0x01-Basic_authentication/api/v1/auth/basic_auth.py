337dae04b6ac352c537cb05f69bde03cd341a632
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-03 05:35:47 +0100

    Extracts base64 path of a auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,16 @@ class BasicAuth(Auth):
     """BasicAuth Class
     """
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the Base64 part of the AUth header
+        Returns:
+            str: Base64 string
+        """
+        if not authorization_header:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        b64 = authorization_header.split(" ")[1]
+        return b64
