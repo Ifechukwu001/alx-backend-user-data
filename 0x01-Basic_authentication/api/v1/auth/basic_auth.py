bc6cbacd69bcd27ee4c1deccbd489df6da914959
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-03 12:24:38 +0100

    Implement for if the password contains colons

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,7 +60,7 @@ class BasicAuth(Auth):
             return None, None
         if ":" not in decoded_base64_authorization_header:
             return None, None
-        usermail, password = decoded_base64_authorization_header.split(":")
+        usermail, password = decoded_base64_authorization_header.split(":", maxsplit=1)
         return usermail, password
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
