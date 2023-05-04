4c12516c3f140bc6f12322d64a04c42b7e5bdbe3
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-04 22:22:16 +0100

    Session authentication route implemented

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Session authentication views
+"""
+from api.v1.views import app_views
+from flask import request, jsonify, make_response
+from models.user import User
+from os import getenv
+
+
+@app_views.route("/auth_session/login",
+                 methods=["POST"], strict_slashes=False)
+def session_login():
+    """Authenticates a new session"""
+    email = request.form.get("email")
+    if not email:
+        return jsonify({ "error": "email missing" }), 400
+    password = request.form.get("password")
+    if not password:
+        return jsonify({ "error": "password missing" }), 400
+    users = User.search({"email": email})
+    if not len(users):
+        return jsonify({ "error": "no user found for this email" }), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = make_response(user.to_json())
+            response.set_cookie(getenv("SESSION_NAME"), session_id)
+            return response
+    return jsonify({ "error": "wrong password" }), 401
