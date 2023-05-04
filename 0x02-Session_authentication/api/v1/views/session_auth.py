28fbfc886746a8c370ccfc01cc9873b8f2dd6f9c
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-04 22:51:17 +0100

    Implement deleting a session id

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,7 @@
 """Session authentication views
 """
 from api.v1.views import app_views
-from flask import request, jsonify, make_response
+from flask import request, jsonify, make_response, abort
 from models.user import User
 from os import getenv
 
@@ -28,3 +28,13 @@ def session_login():
             response.set_cookie(getenv("SESSION_NAME"), session_id)
             return response
     return jsonify({ "error": "wrong password" }), 401
+
+
+@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
+def delete_session():
+    """Deletes a session
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({})
