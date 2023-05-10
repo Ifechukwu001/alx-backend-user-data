0824d997847557cf1b6b7b741356e111c20e0588
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-10 22:31:04 +0100

    Implement method to validate a user password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """Module containing the authentication
 """
 from sqlalchemy.orm.exc import NoResultFound
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 
@@ -26,6 +26,18 @@ class Auth:
         else:
             raise ValueError(f"User {user.email} already exists.")
         return user
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates a user
+        Returns:
+            bool: True if user password matches
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        else:
+            return checkpw(password.encode("utf-8"), user.hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
