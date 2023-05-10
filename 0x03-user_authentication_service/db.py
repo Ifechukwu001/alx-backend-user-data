37248eee383c34a80cc973d2bc2ae6412bc4197b
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-10 21:02:44 +0100

    Implement method to update attributes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", )
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -41,7 +41,7 @@ class DB:
         self._session.commit()
         return user
     
-    def find_user_by(self, *args, **kwargs) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Finds user
         Returns:
             User: User object
@@ -55,3 +55,13 @@ class DB:
         if not result:
             raise NoResultFound
         return result
+
+    def update_user(self, user_id: int, **kwargs):
+        """Updates a User"""
+        user = self.find_user_by(id=user_id)
+        for attr, value in kwargs.items():
+            if attr not in user.__dict__:
+                raise ValueError
+            setattr(user, attr, value)
+        self._session.add(user)
+        self._session.commit()
