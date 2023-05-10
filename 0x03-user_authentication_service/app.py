678643282f2244e05eee68992e85cbd442f48ac7
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-10 22:16:39 +0100

    Implement registering of users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Module containing the Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -10,6 +13,19 @@ app = Flask(__name__)
 def home():
     """Home route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Registers a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"email": user.email, "message": "user created"})
 
 
 if __name__ == "__main__":
