192cc76616e838faa482c93828116eee44dd1516
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-10 23:11:45 +0100

    Implement getting user by session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,6 +54,20 @@ class Auth:
             self._db._session.add(user)
             self._db._session.commit()
             return user.session_id
+        
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Gets a User from its session_id
+        Returns:
+            User: User object
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
 
 
 def _hash_password(password: str) -> bytes:
